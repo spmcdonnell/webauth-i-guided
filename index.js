9459721d3a4208560293210bf6fa544a136ff0cc
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-09-16 13:18:03 -0400

    Fix missing parenth

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ server.post('/api/login', (req, res) => {
     Users.findBy({ username })
         .first()
         .then(user => {
-            if (user && bcrypt.compareSync(password, user.password) {
+            if (user && bcrypt.compareSync(password, user.password)) {
                 res.status(200).json({ message: `Welcome ${user.username}!` });
             } else {
                 res.status(401).json({ message: 'Invalid Credentials' });

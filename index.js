8d46bf6e8292dd87930a70c3f386183aa43cecca
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-09-16 12:42:21 -0400

    Add hashing to name value added through query string

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-model.js');
@@ -12,44 +13,51 @@ server.use(express.json());
 server.use(cors());
 
 server.get('/', (req, res) => {
-  res.send("It's alive!");
+    res.send("It's alive!");
 });
 
 server.post('/api/register', (req, res) => {
-  let user = req.body;
+    let user = req.body;
 
-  Users.add(user)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
 });
 
 server.post('/api/login', (req, res) => {
-  let { username, password } = req.body;
+    let { username, password } = req.body;
 
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user) {
+                res.status(200).json({ message: `Welcome ${user.username}!` });
+            } else {
+                res.status(401).json({ message: 'Invalid Credentials' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
 });
 
 server.get('/api/users', (req, res) => {
-  Users.find()
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => res.send(err));
+    Users.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => res.send(err));
+});
+
+server.get('/hash', (req, res) => {
+    const name = req.query.name;
+    // hash the name
+    const hash = bcrypt.hashSync(name, 14); // use bcryptjs to hash the name
+    res.send(`the hash for ${name} is ${hash}`);
 });
 
 const port = process.env.PORT || 5000;
